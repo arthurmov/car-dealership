2b00f9c64fb525fd038176721507fbf4e68bdad1
arthurmov <officialamovsesyan@gmail.com>
2025-05-09 13:54:26 -0400

    Finished Phase 1

---
+++ src/main/java/com/pluralsight/Dealership.java
@@ -0,0 +1,82 @@
+package com.pluralsight;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Dealership {
+    //will hold information about the dealership (name, address, ...)
+    //and maintain a list of vehicles. Since it has the list of vehicles, it will also
+    //have the methods that search the list for matching vehicles as well as
+    // add/remove vehicles.
+    private String name;
+    private String address;
+    private String phone;
+
+    private ArrayList<Vehicle> inventory;
+
+    public Dealership(String name, String address, String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public List<Vehicle> getVehiclesByPrice(double min, double max) {
+        return null;
+    }
+
+    public List<Vehicle> getVehiclesByMakeModel(String make, String model) {
+        return null;
+    }
+
+    public List<Vehicle> getVehiclesByYear(int min, int max) {
+        return null;
+    }
+
+    public List<Vehicle> getVehiclesByColor(String color) {
+        return null;
+    }
+
+    public List<Vehicle> getVehiclesByMileage(int min, int max) {
+        return null;
+    }
+
+    public List<Vehicle> getVehiclesByType(String vehicleType) {
+        return null;
+    }
+
+    public List<Vehicle> getAllVehicles() {
+        return null;
+    }
+
+    public void addVehicle(Vehicle vehicle) {
+
+    }
+
+    public void removeVehicle(Vehicle vehicle) {
+
+    }
+}
---
+++ src/main/java/com/pluralsight/DealershipFileManager.java
@@ -0,0 +1,8 @@
+package com.pluralsight;
+
+public class DealershipFileManager {
+    //will be responsible for reading the dealership file,
+    //parsing the data, and creating a Dealership object full of vehicles from the
+    //file. It will also be responsible for saving a dealership and the vehicles back
+    //into the file in the same pipe-delimited format
+}
