af14f64deb7cd6582edabd13dab9dd86e6913ab1
arthurmov <officialamovsesyan@gmail.com>
2025-05-14 08:39:55 -0400

    Finished phase 5

--- src/main/java/com/pluralsight/Program.java
+++ src/main/java/com/pluralsight/Program.java
@@ -1,9 +1,9 @@
 package com.pluralsight;
 
-public class Program {
+public class Main {
     //will be responsible for starting the application via its main()
     //method and then creating the user interface and getting it started
-    
+
     public static void main(String[] args) {
         System.out.println("Hello, World!");
 
